a53c944ca541d86d86fd7686cbfbc6c24e69b65f
Gabriel <gsm230508@gmail.com>
2025-06-11 09:19:19 -0300

    ki

---
+++ SunnyLand/Assets/Scripts/PowerUP.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUP : MonoBehaviour
+{
+    private int pontos = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("começou"+ pontos);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("rodando");
+    }
+}
---
+++ SunnyLand/Assets/Scripts/Tails.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tails : MonoBehaviour
+{
+    private string name = "Tails";
+    private int vida = 5;
+    private float velocidade = 100;
+    private bool alive = true;
+    private float gravidade = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("vai filhão"+ name+ vida+ velocidade+ alive);
+        rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = gravidade
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
